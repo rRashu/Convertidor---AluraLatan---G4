ad5ecb156b004ce99d853a4b8e8d599e03e71baa
Robinson <robinsonstalinr@gmail.com>
2023-03-10 22:17:27 -0500

    Convertidor Alura

---
+++ src/Convertidor_Alura/Main.java
@@ -0,0 +1,9 @@
+package Convertidor_Alura;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main (String[] args) {
+        JOptionPane.showInputDialog(null,"Convertidor","Alura curso", 1);
+    }
+}
